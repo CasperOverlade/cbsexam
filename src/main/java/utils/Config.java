8b3a00c1ca1e2a602efe6999920161dc3ea69a60
casperrasmussen <casper.rasmussen@icloud.com>
2018-11-08 13:40:03 +0100

    Added better code and stored it somewhere better

--- src/main/java/utils/Config.java
+++ src/main/java/utils/Config.java
@@ -22,8 +22,13 @@ public final class Config {
   private static long PRODUCT_TTL;
   private static long ORDER_TTL;
   private static long USER_TTL;
+  private static char [] ENCRYPT_KEYWORD;
 
-  public static long getUserTtl(){return USER_TTL;}
+    public static char[] getEncryptKeyword() {
+        return ENCRYPT_KEYWORD;
+    }
+
+    public static long getUserTtl(){return USER_TTL;}
 
   public static long getOrderTtl(){ return ORDER_TTL;}
 
@@ -107,5 +112,6 @@ public final class Config {
     PRODUCT_TTL = json.get("PRODUCT_TTL").getAsLong();
     ORDER_TTL = json.get("ORDER_TTL").getAsLong();
     USER_TTL = json.get("USER_TTL").getAsLong();
+    ENCRYPT_KEYWORD = json.get("ENCRYPT_KEYWORD").getAsString().toCharArray();
   }
 }
