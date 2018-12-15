99856f7ff59bdf36752c6811249364161e0fdb1f
casperrasmussen <casper.rasmussen@icloud.com>
2018-12-15 21:00:08 +0100

    Fixed a bug

--- src/main/java/utils/Token.java
+++ src/main/java/utils/Token.java
@@ -25,7 +25,6 @@ public final class Token {
                     .withIssuer("auth0")
                     .withIssuedAt(new Date(System.currentTimeMillis()))
                     .withExpiresAt(new Date(System.currentTimeMillis() + 900000)) // equals 15 minutes
-                    .withClaim("sub", user.getId())
                     .withSubject(Integer.toString(user.getId()))
                     .sign(algorithm);
 
@@ -48,6 +47,8 @@ public final class Token {
                     .build(); //Reusable verifier instance
 
             verifier.verify(token);
+
+            return true;
 
         } catch (JWTVerificationException exception) {
             //Invalid signature/claims
