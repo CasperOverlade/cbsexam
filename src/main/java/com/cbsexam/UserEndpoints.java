e9ddd59c7a76db8c7baabc8f2ac6d079e7f493cb
casperrasmussen <casper.rasmussen@icloud.com>
2018-12-15 17:34:08 +0100

    Changed some things

--- src/main/java/com/cbsexam/UserEndpoints.java
+++ src/main/java/com/cbsexam/UserEndpoints.java
@@ -15,8 +15,9 @@ import utils.Token;
 @Path("user")
 public class UserEndpoints {
 
+  //cashen vi gemmer brugere i
   private static UserCache userCache = new UserCache();
-  public  boolean forceUpdate=true;
+  public  boolean forceUpdate = true;
 
   /**
    * @param idUser
@@ -113,13 +114,14 @@ public class UserEndpoints {
       // Return the user with the status code 200 if succesful or 401 if failed
       if (user != null) {
           //Welcoming the user and providing him/her with the token they need in order to delete or update their user.
-          String msg = "Hi again "+user.getFirstname() + "! You are logged on and will now receive a token - please save it" +
-                  "as you will need it throughout the system. Here is your token:\n\n"+user.getToken() + "\n\nShould you" +
+          String msg = "Hi again " + user.getFirstname() + "! You are logged on and will now receive a token - please save it" +
+                  "as you will need it throughout the system. Here is your token:\n\n" + user.getToken() + "\n\nShould you" +
                   "loose your token, you can always log in again";
           return Response.status(200).type(MediaType.APPLICATION_JSON_TYPE).entity(msg).build();
       } else {
           return Response.status(401).entity("We could not find the user or it does not exist - please try again").build();
       }
+  }
 
   // TODO: Make the system able to delete users : FIX
   @DELETE
@@ -127,30 +129,31 @@ public class UserEndpoints {
   @Consumes(MediaType.APPLICATION_JSON)
       public Response deleteUser(@PathParam("idUser") int idUser, String body) {
 
-          //Setting a user from the information - note the changes to userobject - we have added token as a instance variable
-          User userToDelete = new Gson().fromJson(body, User.class);
+      //Setting a user from the information - note the changes to userobject - we have added token as a instance variable
+      User userToDelete = new Gson().fromJson(body, User.class);
 
-          // Write to log that we are here
-          Log.writeLog(this.getClass().getName(), this, "Deleting a user", 0);
+      // Write to log that we are here
+      Log.writeLog(this.getClass().getName(), this, "Deleting a user", 0);
 
-          // Use the ID and token to first verify the possibly delete the user from the database via controller.
-          if (Token.verifyToken(userToDelete.getToken(), userToDelete)) {
-              boolean deleted = UserController.deleteUser(idUser);
+      // Use the ID and token to first verify the possibly delete the user from the database via controller.
+      if (Token.verifyToken(userToDelete.getToken(), userToDelete)) {
+          boolean deleted = UserController.deleteUser(idUser);
 
-              //if user was deleted we need to force an update on cache and let the user know it was successfull with status 200
-              //and a message
-              if (deleted) {
-                  forceUpdate = true;
-                  // Return a response with status 200 and a massage
-                  return Response.status(200).type(MediaType.APPLICATION_JSON_TYPE).entity("User deleted").build();
-              } else {
-                  // Return a response with status 200 and a message
-                  return Response.status(400).entity("Could not delete the user - try again").build();
-              }
+          //if user was deleted we need to force an update on cache and let the user know it was successfull with status 200
+          //and a message
+          if (deleted) {
+              forceUpdate = true;
+              // Return a response with status 200 and a massage
+              return Response.status(200).type(MediaType.APPLICATION_JSON_TYPE).entity("User deleted").build();
           } else {
-              //If the token verifier does not check out.
-              return Response.status(401).entity("You're not authorized to do this").build();
+              // Return a response with status 200 and a message
+              return Response.status(400).entity("Could not delete the user - try again").build();
           }
+      } else {
+          //If the token verifier does not check out.
+          return Response.status(401).entity("You're not authorized to do this").build();
+      }
+  }
 
   // TODO: Make the system able to update users : FIX
   @PUT
